663f06876bda4944bd630747dc290304bf154535
jasonthorne <jasonthorne@live.ie>
2020-01-24 23:53:02 +0000

    testing...

--- Workspace/src/com/brs/PairingTest.java
+++ Workspace/src/com/brs/PairingTest.java
@@ -44,14 +44,26 @@ public class PairingTest {
 		//3. create a list of playerOnes for selecting opponents from:
 		List<String>playerOnes = new ArrayList<String>(players);
 		
-		///System.out.println("playerOnes:" + playerOnes);
+		//System.out.println("playerOnes:" + playerOnes);
 		
 		int pairNum = players.size()/2;
 		
 		for(int i=0; i<pairNum;i++) {
 			
 			//4 - randomly select a playerOne from list of playerOnes:
-			String playerOne = playerOnes.get(new Random().nextInt(playerOnes.size())); 
+			
+			//String playerOne = playerOnes.get(new Random().nextInt(playerOnes.size())); 
+			//String playerOne = null;
+			Random rand = new Random();
+			
+			//if(playerOnes.get(new Random().nextInt(playerOnes.size())) == null){
+			while(playerToNextOpps.get(playerOnes.get(rand.nextInt(playerOnes.size()))) == null){
+				System.out.println("REMOVED RAND ******************************");
+				playerOnes.remove(rand.nextInt(playerOnes.size()));
+				rand = new Random();
+			}
+			
+			String playerOne = playerOnes.get(rand.nextInt(playerOnes.size()));
 			
 			//5 - randomly select a playerTwo from playerOne's opponents list in playerToOpponents:
 			List<String>playerTwos = playerToNextOpps.get(playerOne);
@@ -83,25 +95,20 @@ public class PairingTest {
 			//===========================
 			Comparator<String>playerToNextOppsComparator=(opp1, opp2)-> playerToNextOpps.get(opp1).size() - playerToNextOpps.get(opp2).size();
 			
-			Optional<String>playerTwoWithMostOpps = playerTwos.stream().collect(Collectors.maxBy(playerToNextOppsComparator));
-			String test =  "no max opps";
-			
-			System.out.println("player2 with the most opponents: " + playerTwoWithMostOpps.get()); //playerTwoWithMostOpps.get());
-			
-			
+		
 			System.out.println("player2s before removal: " + playerTwos);
 			
 			//list with player twos sorted by the amount of their own next opps 
-			List<String>sortedP = playerTwos.stream().sorted(playerToNextOppsComparator.reversed()).collect(Collectors.toList());
+			List<String>sortedPlayerTwos = playerTwos.stream().sorted(playerToNextOppsComparator.reversed()).collect(Collectors.toList());
 			
-			if (sortedP.size() > 1) {
-				if (playerToNextOpps.get(sortedP.get(0)).size() > playerToNextOpps.get(sortedP.get(1)).size()) {
-					System.out.println("hi there!");
-					playerTwos.remove(sortedP.get(0));
+			if (sortedPlayerTwos.size() > 1) {
+				if (playerToNextOpps.get(sortedPlayerTwos.get(0)).size() > playerToNextOpps.get(sortedPlayerTwos.get(1)).size()) {
+					System.out.println("**********hi there!********");
+					playerTwos.remove(sortedPlayerTwos.get(0));
 				}
 			}
 			
-			System.out.println("sortedP after removal: " + sortedP);
+			//System.out.println("sortedP after removal: " + sortedPlayerTwos);
 			System.out.println("player2s after removal: " + playerTwos);
 			//==========================
 			
@@ -120,6 +127,7 @@ public class PairingTest {
 			
 			//7 - remove playerOne & playerTwo from playerOnes (to prevent re-selection):
 			playerOnes.removeAll(Arrays.asList(playerOne, playerTwo));
+			System.out.println("playerOnes:" + playerOnes);
 			
 			//8 - remove playerOne & playerTwo from all playerToOpponents values:
 			playerToNextOpps.values().forEach(oppsList ->{
@@ -155,9 +163,12 @@ public class PairingTest {
 	
 	
 	
+	/*
+	Optional<String>playerTwoWithMostOpps = playerTwos.stream().collect(Collectors.maxBy(playerToNextOppsComparator));
+	String test =  "no max opps";
 	
-	
-	
+	System.out.println("player2 with the most opponents: " + playerTwoWithMostOpps.get()); //playerTwoWithMostOpps.get());
+	*/
 	
 	
 	
