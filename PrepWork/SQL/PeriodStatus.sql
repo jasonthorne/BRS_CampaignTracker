efe593d3603a3574f885a98272df47ec496f8104
jasonthorne <jasonthorne@live.ie>
2020-04-26 23:33:58 +0100

    raf_planes

--- PrepWork/SQL/PeriodStatus.sql
+++ PrepWork/SQL/PeriodStatus.sql
@@ -1,4 +1,43 @@
 
+/* every potential status for each period of history covered (eg: early, 1940, limit) */
+/*
+
+VERSION WITHOUT USING PERIODS:
+CREATE TABLE period_status (
+  period_statusID int NOT NULL AUTO_INCREMENT,
+  blockID int,
+  yearID int,
+  statusID int,
+  PRIMARY KEY (period_statusID),
+  FOREIGN KEY (blockID) REFERENCES blocks(blockID),
+  FOREIGN KEY (yearID) REFERENCES years(yearID),
+  FOREIGN KEY (statusID) REFERENCES status(statusID)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+INSERT INTO period_status (blockID, yearID, statusID) VALUES (
+	(SELECT blockID FROM blocks WHERE blocks.name = 'Early'),
+	(SELECT yearID FROM years WHERE years.name = '1940'),
+	(SELECT statusID FROM status WHERE status.name = 'None'));
+*/
+
+
+
+
+/*
+INSERT INTO period_status2 (periodID, statusID) VALUES (
+	(SELECT periodID FROM periods 
+	JOIN blocks ON blocks.blockID = periods.periodID
+	JOIN years ON years.yearID = periods.yearID 
+	WHERE blocks.name = 'Early' AND years.name = '1940'),
+	(SELECT statusID FROM status WHERE status.name = 'None'));
+
+*/
+
+
+
+
+
 /* Early 1940 */
 INSERT INTO period_status (blockID, yearID, statusID) VALUES (
 	(SELECT blockID FROM blocks WHERE blocks.name = 'Early'),
--- PrepWork/SQL/SchemaSetup.sql
+++ PrepWork/SQL/SchemaSetup.sql
@@ -8,7 +8,6 @@ CREATE DATABASE blood_red_skies_db;
 USE blood_red_skies_db;
 
 /* airforces involved */
-/*
 CREATE TABLE airforces( 
   airforceID int NOT NULL AUTO_INCREMENT,
   name varchar(64) DEFAULT NULL,
@@ -21,10 +20,9 @@ INSERT INTO airforces (name, description) VALUES ('Luftwaffe','Luftwaffe descrip
 INSERT INTO airforces (name, description) VALUES ('USAAF','USAAF description');
 INSERT INTO airforces (name, description) VALUES ('VVS','VVS description');
 INSERT INTO airforces (name, description) VALUES ('IJAAF','IJAAF description');
-*/
+
 
 /* planes involved */
-/*
 CREATE TABLE planes (
   planeID int NOT NULL AUTO_INCREMENT,
   name varchar(64) DEFAULT NULL,
@@ -36,7 +34,7 @@ INSERT INTO planes (name, description) VALUES ('Spitfire II','Spitfire II descri
 INSERT INTO planes (name, description) VALUES ('Hurricane I','Hurricane I description');
 INSERT INTO planes (name, description) VALUES ('Bf109 E','Bf109 E description');
 INSERT INTO planes (name, description) VALUES ('Bf110 C','Bf110 C description');
-*/
+
 
 /* years covered */
 CREATE TABLE years (
@@ -128,26 +126,8 @@ INSERT INTO status (name) VALUES ('Limit');
 INSERT INTO status (name) VALUES ('Auto');
 
 
-/* every potential status for each period of history covered (eg: early, 1940, limit) */
+/* each potential status for each period of history covered (eg: early, 1940, limit) */
 CREATE TABLE period_status (
-  period_statusID int NOT NULL AUTO_INCREMENT,
-  blockID int,
-  yearID int,
-  statusID int,
-  PRIMARY KEY (period_statusID),
-  FOREIGN KEY (blockID) REFERENCES blocks(blockID),
-  FOREIGN KEY (yearID) REFERENCES years(yearID),
-  FOREIGN KEY (statusID) REFERENCES status(statusID)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
-/* Early 1940 */
-INSERT INTO period_status (blockID, yearID, statusID) VALUES (
-	(SELECT blockID FROM blocks WHERE blocks.name = 'Early'),
-	(SELECT yearID FROM years WHERE years.name = '1940'),
-	(SELECT statusID FROM status WHERE status.name = 'None'));
-
-
-CREATE TABLE period_status2 (
   period_statusID int NOT NULL AUTO_INCREMENT,
   periodID int,
   statusID int,
@@ -158,26 +138,46 @@ CREATE TABLE period_status2 (
 
 
 /* Early, 1940, None */
-
-INSERT INTO period_status2 (periodID, statusID) VALUES (
+INSERT INTO period_status (periodID, statusID) VALUES (
 	(SELECT periodID FROM periods 
-	JOIN blocks ON blocks.blockID = periods.periodID
-	JOIN years ON years.yearID = periods.yearID 
-	WHERE blocks.name = 'Early' AND years.name = '1940'),
-	(SELECT statusID FROM status WHERE status.name = 'None'));
-
-/* --------------------*/
-INSERT INTO period_status2 (periodID, statusID) VALUES (
-	(
-		SELECT periodID FROM periods 
 		INNER JOIN blocks ON blocks.blockID = periods.blockID
 		INNER JOIN years ON years.yearID = periods.yearID 
-		WHERE blocks.name = 'Late' AND years.name = '1940'
-	),
+		WHERE blocks.name = 'Early' AND years.name = '1940'),
 	(SELECT statusID FROM status WHERE status.name = 'None'));
 	
+/* Mid, 1941, None */
+INSERT INTO period_status (periodID, statusID) VALUES (
+	(SELECT periodID FROM periods 
+		INNER JOIN blocks ON blocks.blockID = periods.blockID
+		INNER JOIN years ON years.yearID = periods.yearID 
+		WHERE blocks.name = 'Mid' AND years.name = '1941'),
+	(SELECT statusID FROM status WHERE status.name = 'Auto'));
 
 
-
-
+/* RAF planes */
+CREATE TABLE raf_planes (
+	raf_planeID int NOT NULL AUTO_INCREMENT,
+	planeID int,
+	airforceID int,
+	period_statusID_early_1940 int,	/* 1940 */
+	period_statusID_mid_1940 int,
+	period_statusID_late_1940 int,
+	period_statusID_early_1941 int, /* 1941 */
+	period_statusID_mid_1941 int,
+	period_statusID_late_1941 int,
+	period_statusID_early_1942 int, /* 1942 */
+	period_statusID_mid_1942 int,
+	period_statusID_late_1942 int,
+	period_statusID_early_1943 int, /* 1943 */
+	period_statusID_mid_1943 int,
+	period_statusID_late_1943 int,
+	period_statusID_early_1944 int, /* 1944 */
+	period_statusID_mid_1944 int,
+	period_statusID_late_1944 int,
+	period_statusID_early_1945 int, /* 1945 */
+	period_statusID_mid_1945 int,
+	PRIMARY KEY (raf_planeID),
+	FOREIGN KEY (planeID) REFERENCES planes(planeID),
+	FOREIGN KEY (statusID) REFERENCES status(statusID)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
