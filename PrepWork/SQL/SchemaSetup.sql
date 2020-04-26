85d5ad8eca3ad120b90285f669e8c99ef6330c63
jasonthorne <jasonthorne@live.ie>
2020-04-26 17:42:51 +0100

    Periods

--- PrepWork/SQL/SchemaSetup.sql
+++ PrepWork/SQL/SchemaSetup.sql
@@ -1,9 +1,14 @@
-create database if not exists blood_red_skies_db;
 
-use blood_red_skies_db;
+/*CREATE database if not exists blood_red_skies_db;*/
+
+DROP DATABASE blood_red_skies_db;
+
+CREATE DATABASE blood_red_skies_db;
+
+USE blood_red_skies_db;
 
 /* airforces involved */
-drop table if exists airforces;
+/*
 CREATE TABLE airforces( 
   airforceID int NOT NULL AUTO_INCREMENT,
   name varchar(64) DEFAULT NULL,
@@ -16,9 +21,10 @@ INSERT INTO airforces (name, description) VALUES ('Luftwaffe','Luftwaffe descrip
 INSERT INTO airforces (name, description) VALUES ('USAAF','USAAF description');
 INSERT INTO airforces (name, description) VALUES ('VVS','VVS description');
 INSERT INTO airforces (name, description) VALUES ('IJAAF','IJAAF description');
+*/
 
 /* planes involved */
-drop table if exists planes;
+/*
 CREATE TABLE planes (
   planeID int NOT NULL AUTO_INCREMENT,
   name varchar(64) DEFAULT NULL,
@@ -30,9 +36,9 @@ INSERT INTO planes (name, description) VALUES ('Spitfire II','Spitfire II descri
 INSERT INTO planes (name, description) VALUES ('Hurricane I','Hurricane I description');
 INSERT INTO planes (name, description) VALUES ('Bf109 E','Bf109 E description');
 INSERT INTO planes (name, description) VALUES ('Bf110 C','Bf110 C description');
+*/
 
 /* years covered */
-drop table if exists years;
 CREATE TABLE years (
   yearID int NOT NULL AUTO_INCREMENT,
   name varchar(4) DEFAULT NULL, 
@@ -48,7 +54,6 @@ INSERT INTO years (name) VALUES ('1945');
 
 
 /* blocks of a year (early, mid, late) */
-drop table if exists blocks;
 CREATE TABLE blocks (
   blockID int NOT NULL AUTO_INCREMENT,
   name varchar(5) DEFAULT NULL, 
@@ -59,9 +64,32 @@ INSERT INTO blocks (name) VALUES ('Early');
 INSERT INTO blocks (name) VALUES ('Mid');
 INSERT INTO blocks (name) VALUES ('Late');
 
+/* historical periods (eg: early 1940) */
+CREATE TABLE periods (
+  periodID int NOT NULL AUTO_INCREMENT,
+  blockID int,
+  yearID int,
+  PRIMARY KEY (periodID),
+  FOREIGN KEY (blockID) REFERENCES blocks(blockID),
+  FOREIGN KEY (yearID) REFERENCES years(yearID)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+/* =========================== */
+INSERT INTO periods (blockID, yearID) VALUES (
+	(SELECT blockID FROM blocks WHERE blocks.name = 'Early'),
+	(SELECT yearID FROM years WHERE years.name = '1940'));
+
+INSERT INTO periods (blockID, yearID) VALUES (
+	(SELECT blockID FROM blocks WHERE blocks.name = 'Mid'),
+	(SELECT yearID FROM years WHERE years.name = '1940'));
+
+INSERT INTO periods (blockID, yearID) VALUES (
+	(SELECT blockID FROM blocks WHERE blocks.name = 'Late'),
+	(SELECT yearID FROM years WHERE years.name = '1940'));
+/* =========================== */	
+	
 
 /* status values of a plane (none, limit, auto) */
-drop table if exists status;
 CREATE TABLE status (
   statusID int NOT NULL AUTO_INCREMENT,
   name varchar(5) DEFAULT NULL,
@@ -74,7 +102,6 @@ INSERT INTO status (name) VALUES ('Auto');
 
 
 /* every potential status for each period of history covered (eg: early, 1940, limit) */
-drop table if exists period_status;
 CREATE TABLE period_status (
   period_statusID int NOT NULL AUTO_INCREMENT,
   blockID int,
@@ -92,50 +119,21 @@ INSERT INTO period_status (blockID, yearID, statusID) VALUES (
 	(SELECT yearID FROM years WHERE years.name = '1940'),
 	(SELECT statusID FROM status WHERE status.name = 'None'));
 
-INSERT INTO period_status (blockID, yearID, statusID) VALUES (
-	(SELECT blockID FROM blocks WHERE blocks.name = 'Early'),
-	(SELECT yearID FROM years WHERE years.name = '1940'),
-	(SELECT statusID FROM status WHERE status.name = 'Limit'));
 
-INSERT INTO period_status (blockID, yearID, statusID) VALUES (
-	(SELECT blockID FROM blocks WHERE blocks.name = 'Early'),
-	(SELECT yearID FROM years WHERE years.name = '1940'),
-	(SELECT statusID FROM status WHERE status.name = 'Auto'));
+CREATE TABLE period_status2 (
+  period_statusID int NOT NULL AUTO_INCREMENT,
+  periodID int,
+  statusID int,
+  PRIMARY KEY (period_statusID),
+  FOREIGN KEY (periodID) REFERENCES periods(periodID),
+  FOREIGN KEY (statusID) REFERENCES status(statusID)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-/* Mid 1940 */
-INSERT INTO period_status (blockID, yearID, statusID) VALUES (
-	(SELECT blockID FROM blocks WHERE blocks.name = 'Mid'),
-	(SELECT yearID FROM years WHERE years.name = '1940'),
+
+/* Early, 1940, None */
+INSERT INTO period_status2 (periodID, statusID) VALUES (
+	(SELECT periodID FROM periods INNER JOIN blocks ON periods WHERE periods.blockID = 'Early'),
 	(SELECT statusID FROM status WHERE status.name = 'None'));
-
-INSERT INTO period_status (blockID, yearID, statusID) VALUES (
-	(SELECT blockID FROM blocks WHERE blocks.name = 'Mid'),
-	(SELECT yearID FROM years WHERE years.name = '1940'),
-	(SELECT statusID FROM status WHERE status.name = 'Limit'));
-
-INSERT INTO period_status (blockID, yearID, statusID) VALUES (
-	(SELECT blockID FROM blocks WHERE blocks.name = 'Mid'),
-	(SELECT yearID FROM years WHERE years.name = '1940'),
-	(SELECT statusID FROM status WHERE status.name = 'Auto'));
-
-/* Late 1940 */
-INSERT INTO period_status (blockID, yearID, statusID) VALUES (
-	(SELECT blockID FROM blocks WHERE blocks.name = 'Late'),
-	(SELECT yearID FROM years WHERE years.name = '1940'),
-	(SELECT statusID FROM status WHERE status.name = 'None'));
-
-INSERT INTO period_status (blockID, yearID, statusID) VALUES (
-	(SELECT blockID FROM blocks WHERE blocks.name = 'Late'),
-	(SELECT yearID FROM years WHERE years.name = '1940'),
-	(SELECT statusID FROM status WHERE status.name = 'Limit'));
-
-INSERT INTO period_status (blockID, yearID, statusID) VALUES (
-	(SELECT blockID FROM blocks WHERE blocks.name = 'Late'),
-	(SELECT yearID FROM years WHERE years.name = '1940'),
-	(SELECT statusID FROM status WHERE status.name = 'Auto'));
-
-/*----------------*/
-
 
 
 
