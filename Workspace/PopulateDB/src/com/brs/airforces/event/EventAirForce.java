b4f6aefb0edd72bb301c6ce60057421843c49462
jasonthorne <jasonthorne@live.ie>
2020-05-06 01:47:06 +0100

    hmmmm...

--- Workspace/PopulateDB/src/com/brs/airforces/event/EventAirForce.java
+++ Workspace/PopulateDB/src/com/brs/airforces/event/EventAirForce.java
@@ -1,7 +1,24 @@
 package com.brs.airforces.event;
 
+import com.brs.airforces.AirForce;
+import com.brs.events.Event;
+
 public class EventAirForce {
 	
+	private final Event event; //event
+	private final AirForce airForce; //air force
+	private final boolean hasHomeAdvantage; //home advantage status
+	
+	protected EventAirForce(Event event, AirForce airForce, boolean hasHomeAdvantage) {
+		this.event = event;
+		this.airForce = airForce;
+		this.hasHomeAdvantage = hasHomeAdvantage;
+	}
+
+	@Override
+	public String toString() {
+		return "EventAirForce [event=" + event + ", airForce=" + airForce + ", hasHomeAdvantage=" + hasHomeAdvantage + "]";
+	}
 	
 
 }
--- Workspace/PopulateDB/src/com/brs/airforces/event/EventAirForces.java
+++ Workspace/PopulateDB/src/com/brs/airforces/event/EventAirForces.java
@@ -1,7 +1,18 @@
 package com.brs.airforces.event;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.brs.events.Event;
+
 public class EventAirForces {
 	
+	private final Map<Event, List<EventAirForce>>event_to_airForces = new HashMap<Event, List<EventAirForce>>();
+	
+	
+	//Battle of Britian:
 	
 
 }
