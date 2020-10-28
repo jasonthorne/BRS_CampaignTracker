c137f39fde4d99c66f1ed7bc0114cd638141d871
jasonthorne <jasonthorne@live.ie>
2020-10-28 19:52:32 +0000

    cleanup frameController

--- Workspace/CampaignTracker/src/controller/FrameController.java
+++ Workspace/CampaignTracker/src/controller/FrameController.java
@@ -63,43 +63,29 @@ public class FrameController implements Rootable, Fadeable {
     private final Stage stage = new Stage(); //stage
     private Scene scene; //scene
     
+    //singleton frame.fxml controller:
+  	private static FrameController singleFrameCtrlr = null;
+    
     //login.fxml controller:
     private final LoginController loginCtrlr;
    
-    //stacks of parent nodes. One for forward moves, one for backward moves:
-  	private final Stack<Frameable>fwrdMoves = new Stack<Frameable>(); 
-  	private final Stack<Frameable>bkwrdMoves = new Stack<Frameable>();
+    //stacks for navigating controllers who's views are added to this controller's view: 
+  	private final Stack<Frameable>fwrdMoves = new Stack<Frameable>(); //records forward moves
+  	private final Stack<Frameable>bkwrdMoves = new Stack<Frameable>(); //records backward moves
   	
-  	//private final Stack<Parent>fwrdMoves = new Stack<Parent>(); 
-  	//private final Stack<Parent>bkwrdMoves = new Stack<Parent>();
-  	
-  	//----------------------------
-  	//private final Stack<Frameable>fwrdMoves2 = new Stack<Frameable>();
-  	///private final Stack<Frameable>bkwrdMoves2 = new Stack<Frameable>();
-  	
-  	//------------------------------
-  	
-  	
-  	//===========================================
-  	//singleton frame.fxml controller:
-  	private static FrameController singleFrameCtrlr = null; 
-  	
-  	//gets singleton frame controller:
+  	//constructor:
+    private FrameController(){
+    	loginCtrlr = new LoginController(this); //instantiate login controller //??????????????passing this??
+    	scene = new Scene(Rootable.getRoot(this, FxmlPath.FRAME));
+    	stage.setScene(scene); //add scene to stage
+    }	
+  	 
+  	//get frame controller:
     public static FrameController getFrameCtrlr() {
     	//create singleton if necessary:
         if (singleFrameCtrlr == null) { singleFrameCtrlr = new FrameController(); }
         return singleFrameCtrlr; 
     }
-    
-  	//===========================================
-  	
-    //constructor:
-    //////////////===========public FrameController(){
-    private FrameController(){
-    	loginCtrlr = new LoginController(this); //instantiate login controller 
-    	scene = new Scene(Rootable.getRoot(this, FxmlPath.FRAME));
-    	stage.setScene(scene); //add scene to stage
-    }	
     
     //show stage:
     public void showStage() { stage.showAndWait(); }
@@ -113,14 +99,13 @@ public class FrameController implements Rootable, Fadeable {
   	}
   	
   	//move to first logged in view:
-  	///////////+++++++++++++++++++void loginMove(Parent root) {
 	void loginMove(Frameable frameable) {
-  		fwrdMoves.push(frameable); //mark frameable as fwrd move
+  		fwrdMoves.push(frameable); //mark as forward move
     	addRootToBody(frameable.getRoot()); //add root to bodyAP
+    	setViewLbl(frameable.getViewTitle()); //add view title
   	}
   	
   	//move forward to new view:
-  	////////+++++++++++++/void moveFwrd(Parent root) {
 	void moveFwrd(Frameable frameable) {
   		
   		//turn on back button:
@@ -128,15 +113,14 @@ public class FrameController implements Rootable, Fadeable {
   		
   		//if there are stored bkwrdMoves:
     	if(!bkwrdMoves.isEmpty()) {
-    		/** instead of pop, maybe check if its the same as current pos?????? */
     		bkwrdMoves.pop(); //remove obsolete element (as traversing a new path)
         	//turn off fwrd buttons if all bkwrdMoves are removed:
         	if(bkwrdMoves.isEmpty()) { fwrdBtn.setDisable(true); }
     	}
     	
-    	////////////++++++++++fwrdMoves.push(root); //mark root as fwrd move
-    	fwrdMoves.push(frameable); //mark frameable as fwrd move
+    	fwrdMoves.push(frameable); //mark as forward move
     	addRootToBody(frameable.getRoot()); //add root to bodyAP
+    	setViewLbl(frameable.getViewTitle()); //add view title
 	}
   	
   	//move forward to previous view:
@@ -148,8 +132,8 @@ public class FrameController implements Rootable, Fadeable {
     	//disable fwrd btn if you've reached end of traversed path:
     	if(bkwrdMoves.isEmpty()) { fwrdBtn.setDisable(true); } 
     	
-    	//////////++++++++++addRootToBody(fwrdMoves.peek()); //add root to frame
     	addRootToBody(fwrdMoves.peek().getRoot()); //add root to frame
+    	setViewLbl(fwrdMoves.peek().getViewTitle()); //add view title
 	}
 	
 	//move backwards to previous view:
@@ -162,72 +146,9 @@ public class FrameController implements Rootable, Fadeable {
     	//disable back btn if now at last element in stack:
     	if(fwrdMoves.size() == 1) { backBtn.setDisable(true); }
     		
-    	//////////++++++++++++addRootToBody(fwrdMoves.peek()); //add root to frame
     	addRootToBody(fwrdMoves.peek().getRoot()); //add root to frame
+    	setViewLbl(fwrdMoves.peek().getViewTitle()); //add view title
 	}
-	
-	//========================================================================
-  	/*
-  	//move to first logged in view:
-  	void loginMove(FrameContent content) {
-  		fwrdMoves2.push(content); //mark root as fwrd move
-    	addRootToBody(content.getRoot()); //add root to bodyAP
-    	setViewLbl(content.getViewTitle()); //add view title
-  	}
-  	
-  	//move forward to new view:
-  	void moveFwrd(FrameContent content) {
-  		
-  		//turn on back button:
-  		if(backBtn.isDisabled()) { backBtn.setDisable(false); }
-  		
-  		//if there are stored bkwrdMoves:
-    	if(!bkwrdMoves2.isEmpty()) {
-    		///////// instead of pop, maybe check if its the same as current pos?????? 
-    		bkwrdMoves2.pop(); //remove obsolete element (as traversing a new path)
-        	//turn off fwrd buttons if all bkwrdMoves are removed:
-        	if(bkwrdMoves2.isEmpty()) { fwrdBtn.setDisable(true); }
-    	}
-    	
-    	fwrdMoves2.push(content); //mark root as fwrd move
-    	addRootToBody(content.getRoot()); //add root to bodyAP
-    	setViewLbl(content.getViewTitle()); //add view title
-	}
-  	
-  	//move forward to previous view:
-	private void moveFwrd() {
-		
-		//turn on back button:
-    	if(backBtn.isDisabled()) { backBtn.setDisable(false); } 
-    	fwrdMoves2.push(bkwrdMoves2.pop()); //return previous view to fwrdMoves
-    	//disable fwrd btn if you've reached end of traversed path:
-    	if(bkwrdMoves2.isEmpty()) { fwrdBtn.setDisable(true); } 
-    	
-    	addRootToBody(fwrdMoves2.peek().getRoot()); //add root to frame
-    	setViewLbl(fwrdMoves2.peek().getViewTitle()); //add view title
-	}
-	
-	//move backwards to previous view:
-	private void moveBkwrd() {
-
-		//turn on fwrd button:
-    	if(fwrdBtn.isDisabled()) { fwrdBtn.setDisable(false); }
-    	//move current fwrd move to bkwrdMoves:
-    	bkwrdMoves2.push(fwrdMoves2.pop()); 
-    	//disable back btn if now at last element in stack:
-    	if(fwrdMoves2.size() == 1) { backBtn.setDisable(true); }
-    		
-    	addRootToBody(fwrdMoves2.peek().getRoot()); //add root to frame
-    	setViewLbl(fwrdMoves2.peek().getViewTitle()); //add view title
-	}
-  	*/
-  	//========================================================================
-	
-	
-	//+++++++++++++++++++++++++++++++
-	//void changeView() { }
-	
-	//++++++++++++++++++++++++++++++
 	
 	//set player name label:
 	void setPlayerLbl(String playerName) {
--- Workspace/CampaignTracker/src/controller/Frameable.java
+++ Workspace/CampaignTracker/src/controller/Frameable.java
@@ -3,46 +3,16 @@ package controller;
 import animation.Fadeable;
 import javafx.scene.Parent;
 
-//public abstract class FrameContent implements Fadeable {
+/** serves controller classes who's views are displayed by adding their root nodes to frame.fxml */
+
 public interface Frameable extends Fadeable {
 	
-	//https://github.com/jasonthorne/BRS_CampaignTracker/tree/e6e35427a6abb839d39adb9d3a7582182f54fb13/Workspace/CampaignTracker/src
-	
-	//frame.fxml controller:
-	//FrameController frameCtrlr;
-
-	
-	//set frame controller:
-	/*default void setFrameController(FrameController frameCtrlr2) {
-		frameCtrlr = frameCtrlr2; //+++++++++++WE WANT THIS FIRING ONLY ONCE!!++++++++++++
-	}*/
-	
-	//get frame controller:
-	/*FrameController getFrameController() {
-		return FrameContent.frameCtrlr;
-	} ++++++++++++++++++++try not to use thiS!! */
-	
-	/*
 	//change to another view:
-	//default void changeView(Parent thisRoot, Parent nextRoot) {
-	default void changeView(Parent thisRoot, Parent nextRoot, FrameController frameCtrlr) {
-		//////frameCtrlr.setViewLbl(viewTitle);
+	static void changeView(Parent thisRoot, Frameable nextFrameable) {
 		Fadeable.fade(thisRoot, FadeOption.FADE_OUT); //fade out this root
-		////=======================frameCtrlr.moveFwrd(nextRoot); //move to next root
-		FrameController.getFrameCtrlr().moveFwrd(nextRoot);
-	}*/
-	
-	
-	
-	//change to another view:
-	static void changeView(Parent thisRoot, Frameable framable) {
-		///////frameCtrlr.setViewLbl(frameContent.getViewTitle());
-		Fadeable.fade(thisRoot, FadeOption.FADE_OUT); //fade out this root
-		//frameCtrlr.moveFwrd(frameContent); //move to next root
-		FrameController.getFrameCtrlr().moveFwrd(framable);
+		FrameController.getFrameCtrlr().moveFwrd(nextFrameable); //move to nextFrameable
 	}
 	
-		
 	//force root getter/setter:
 	abstract void setRoot(); 
 	abstract Parent getRoot();
@@ -50,5 +20,4 @@ public interface Frameable extends Fadeable {
 	//force title getter/setter:
 	abstract void setViewTitle(); 
 	abstract String getViewTitle();	
-	
 }
