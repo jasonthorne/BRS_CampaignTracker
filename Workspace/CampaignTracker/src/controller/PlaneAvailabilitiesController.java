5a31490d5cadf0b24530d4c05864b349d7fcde07
jasonthorne <jasonthorne@live.ie>
2020-11-19 18:09:46 +0000

    test table added

--- Workspace/CampaignTracker/src/controller/PlaneAvailabilitiesController.java
+++ Workspace/CampaignTracker/src/controller/PlaneAvailabilitiesController.java
@@ -4,10 +4,16 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import animation.Fadeable;
+import animation.Fadeable.FadeOption;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.Event;
 import model.Period;
@@ -20,35 +26,67 @@ public class PlaneAvailabilitiesController implements Rootable {
     @FXML
     private URL location;
     
-    @FXML
-    private Label testLbl;
+    //root fxml element & children:
+    @FXML private VBox rootVB;
+  
 
-    
     @FXML
     void initialize() {
-    	testLbl.setText(startPeriod.toString() + " " + endPeriod.toString());
+    	////////testLbl.setText(startPeriod.toString() + " " + endPeriod.toString());
     }
     
-  	private final Stage stage = new Stage(); //stage
-    private final Scene scene; //scene
+    //++++++++++++++++++++++++++++++++++++++
+    
+    //BUG - button needs disabled when table is opened! then enabled on close of table
+    //+++++++++++++++++++++++++++++++++++++
+    
+    //----------------------
+    
+    private TableView table = new TableView();
+    TableColumn firstNameCol = new TableColumn("First Name");
+    TableColumn lastNameCol = new TableColumn("Last Name");
+    TableColumn emailCol = new TableColumn("Email");
+    
+   
+    
+    //---------------------
     
     private final Period startPeriod; //start period
     private final Period endPeriod; //end period
+    
+  	private final Stage stage = new Stage(); //stage
+    private final Scene scene; //scene
     
     //constructor:
 	PlaneAvailabilitiesController(Period start, Period end) {
 		startPeriod = start; //set start period
 		endPeriod = end; //set end period
 		
+		
 		//add root to scene:
 		scene = new Scene(Rootable.getRoot(this, "/view/planeAvailabilities.fxml")); 
-    	stage.setScene(scene); //add scene to stage
+    	stage.setScene(scene); //add scene to stage	
     }
 	
 	
-    void showPlanes(List<Plane>planes){
-    	System.out.println(planes); //+++++++++++++++THESE ARE AIRFORCE_PLANES NOT EVENT_AIRFORCE_PLANES!! +++++++++++
+    void showPlanes(List<Plane>planes){ //airForceName???????????????
+    	//System.out.println(airForceName);
+    	
+    	//----------------------------
+    	
+		table.getColumns().setAll(firstNameCol, lastNameCol, emailCol);
+		rootVB.getChildren().setAll(table);
+		//-----------------------------
+    	
+    	showStage();
     }
+    
+    
+    
+    
+  
+    
+    
 	
     //show stage:
     private void showStage() { stage.showAndWait(); }
