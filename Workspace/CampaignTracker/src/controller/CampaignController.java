6dbc9b1c1c5be9c2ea4812c5999b6ac70b3c92fe
jasonthorne <jasonthorne@live.ie>
2020-12-10 00:03:05 +0000

    bug fix

--- Workspace/CampaignTracker/src/controller/CampaignController.java
+++ Workspace/CampaignTracker/src/controller/CampaignController.java
@@ -1,12 +1,32 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class CampaignController implements Rootable{
 	
-	
-	
+	@FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane testAP;
+
+    @FXML
+    private Label testLbl;
+
+    @FXML
+    void initialize() {
+    
+    }
 	
 	
 	private final Stage stage = new Stage(); //stage
