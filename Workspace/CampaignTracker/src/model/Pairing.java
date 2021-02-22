58151cd2bb4d34d8729f1d05ed29e017bba210ac
jasonthorne <jasonthorne@live.ie>
2021-02-22 13:40:44 +0000

    changed to stack

--- Workspace/CampaignTracker/src/model/Pairing.java
+++ Workspace/CampaignTracker/src/model/Pairing.java
@@ -7,7 +7,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 
+import controller.Frameable;
 import controller.LoginController;
 
 //////////ADAPTED FROM:
@@ -16,31 +18,27 @@ import controller.LoginController;
 public class Pairing {
 	
 	//maps with their own player name key, holding the same pairing:
-	//////////private final Map<String, List<String>> playerOneToPairing = new HashMap<String, List<String>>();
-    ///////////private final Map<String, List<String>> playerTwoToPairing = new HashMap<String, List<String>>();
-    private final Map<String, Set<String>> playerOneToPairing = new HashMap<String, Set<String>>();
-    private final Map<String, Set<String>> playerTwoToPairing = new HashMap<String, Set<String>>();
+    private final Map<String, Stack<String>> playerOneToPairing = new HashMap<String, Stack<String>>();
+    private final Map<String, Stack<String>> playerTwoToPairing = new HashMap<String, Stack<String>>();
     
     //constructor:
     public Pairing(String player1, String player2) {
     	
     	//create pairing holding player names:
-    	//////////List<String> pairing = Arrays.asList(player1, player2);
-    	Set<String> pairing = new HashSet<String>(Arrays.asList(player1, player2));
+    	Stack<String> pairing  = new Stack<String>();
+    	pairing.addAll(Arrays.asList(player1, player2));
     	
     	//add keys with pairing to maps:
     	playerOneToPairing.put(player1, pairing);
     	playerTwoToPairing.put(player2, pairing);
     }
     
-    //try both maps with key, or return empty list:
-    public Set<String> getPairing(String key) {
+    //try both maps with key, or return empty stack:
+    public Stack<String> getPairing(String key) {
     	return playerOneToPairing.getOrDefault(
     			key, playerTwoToPairing.getOrDefault(
-    					/////////key, Collections.emptyList())); //+++++++++++++++REMEMBER TO CHECK FOR EMPTY LIST WHEN CALLING :P
-    					key, Collections.emptySet())); //+++++++++++++++REMEMBER TO CHECK FOR EMPTY LIST WHEN CALLING :P 
+    					key, new Stack<String>()));
     }
-
 
     //return if a map contains given key:
     public boolean getHasKey(String key) {
@@ -50,26 +48,24 @@ public class Pairing {
     
     public void swapPlayer(String oldKey, String newKey) {
     	
-    	Set<String> pairing; //set for holding pairing
+    	Stack<String> pairing; //set for holding pairing
     	
     	//if pairing was found using oldKey:
     	if(!(pairing = this.getPairing(oldKey)).isEmpty()) {
     		
-    		//replace oldKey with newKey in pairing:
-    		//Collections.replaceAll(pairing, oldKey, newKey);
+    		pairing.remove(oldKey); //remove old key
+    		String otherKey = pairing.peek(); //copy other key
+    		pairing.push(newKey); //add new key
     		
-    		pairing.remove(oldKey); //remove old key from list
-    		String otherKey = /////////////?????? SET SHOULD PROB BE A QUEUE :P
-    		
-    		//remove old entry 
+    		//if old key is in playerOne map:
     		if(playerOneToPairing.containsKey(oldKey)) {
-    			
-    			playerOneToPairing.remove(oldKey); //remove entry
+    			playerOneToPairing.remove(oldKey); //remove old entry
     			playerOneToPairing.put(newKey, pairing); //add new entry
-    			playerTwoToPairing.replace(key, value) //replace entry in 2 with new entry (you know what i mean!! :P)
-    		}else { 
-    			playerTwoToPairing.remove(oldKey); //remove entry
+    			playerTwoToPairing.replace(otherKey, pairing); //update playerTwo
+    		} else { 
+    			playerTwoToPairing.remove(oldKey); //remove old entry
     			playerTwoToPairing.put(newKey, pairing); //add new entry
+    			playerOneToPairing.replace(otherKey, pairing); //update playerOne
 			}
     	}
     }
