fa473d26543ca6d2eae0bdc508071ff45c22be2e
jasonthorne <jasonthorne@live.ie>
2021-03-08 20:29:45 +0000

    notification

--- Workspace/CampaignTracker/src/model/Notification.java
+++ Workspace/CampaignTracker/src/model/Notification.java
@@ -2,6 +2,24 @@ package model;
 
 public final class Notification {
 	
+	public enum Status {
+		
+		SELECT_AIRFORCE("Select Air Force"), 
+		PLAY_MATCH("Play Match")
+		/* etc... */;
+		
+		private final String status; //status
+		//constructor:
+		private Status(String status) { this.status = status; } 
+		@Override 
+		public String toString() { return status; } //return status
+	}
+	
+	
+	public Notification(Status status, String path) {
+		
+	}
+	
 	/*
 	 * title
 	 * 
@@ -12,16 +30,7 @@ public final class Notification {
 	 * link
 	 */
 	
-	/*
-	public enum Status {
-		
-		UNAVAILABLE("Unavailable"), LIMIT("Limit"), AUTO("Auto");
-		
-		private final String status; //status
-		//constructor:
-		private Status(String status) { this.status = status; } 
-		@Override 
-		public String toString() { return status; } //return status
-	}*/
+	
+	
 
 }
